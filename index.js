8f81de84a45d2cb0c6f70ce656d4ed25fbbaf89a
FatimaAlmashhor <fatima.amin4official@gmail.com>
2022-02-15 21:33:47 +0300

    Template

--- index.js
+++ index.js
@@ -1,61 +1,93 @@
 const http = require('http');
 const route = require('url');
 const fs = require('fs');
-http.createServer((req, res) => {
-    try {
+const path = require('path')
+const mimetypes = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'js': 'text/javascript',
+    'png': 'image/png',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpg'
+};
 
+
+const file = (fileName) => fs.createReadStream(fileName, 'utf-8');
+
+const renderLinks = (req, res) => {
+    // console.log('[style path ]', req.url);
+    // // console.log();
+    switch (path.extname(req.url)) {
+        case '.css':
+            res.setHeader('Content-Type', 'text/css');
+            file(`./src${req.url}`).pipe(res);
+            break;
+        case '.js':
+            res.setHeader('Content-Type', 'text/javascript');
+            file(`./src${req.url}`).pipe(res);
+
+            break;
+        case '.jpg':
+            res.setHeader('Content-Type', mimetypes['jpg']);
+            file(`./src${req.url}`).pipe(res);
+            break;
+        case '.png':
+            res.setHeader('Content-Type', mimetypes['png']);
+            file(`./src${req.url}`).pipe(res);
+            break;
+        case '.jpeg':
+            res.setHeader('Content-Type', mimetypes['jpeg']);
+            file(`./src${req.url}`).pipe(res);
+            break;
+        default:
+            break
+    }
+
+    // console.log(req.url);
+    // console.log(req.url.startsWith('/assets/css/main.css'));
+    // if (req.url.startsWith('/style.css')) {
+    //     res.setHeader('Content-Type', 'text/css');
+    //     file(`./src${req.url}`).pipe(res);
+    // }
+}
+http.createServer((req, res) => {
+
+    try {
+        renderLinks(req, res)
+        // console.log(req.url);
+        // console.log(req.url.startsWith('/css/style.css'));
+        // if (req.url.startsWith('/css/style.css')) {
+        //     res.setHeader('Content-Type', 'text/css');
+        //     file('./src/css/style.css').pipe(res);
+        // }
         switch (req.url) {
             case '/':
             case '/home':
-
-                fs.readFile(__dirname + '/src/index.html', function (err, html) {
-                    if (err) throw err;
-                    res.writeHeader(200, { "Content-Type": "text/html" });
-                    res.write(html);
-                    res.end()
-                })
-                // fs.readFile(__dirname + '/src/css/style.css', function (err, data) {
-                //     if (err) console.log(err);
-                //     res.writeHead(200, { 'Content-Type': 'text/css' });
-                //     res.write(data);
-                //     res.end();
-                // });
+                res.setHeader("Content-Type", "text/html");
+                file('./src/index.html').pipe(res);
 
                 break;
             case '/blog':
-                fs.readFile('./src/bloges.html', function (err, html) {
-                    if (err) throw err;
-                    res.writeHeader(200, { "Content-Type": "text/html" });
-                    res.write(html);
-                    res.end()
-                })
+                res.setHeader("Content-Type", "text/html");
+                file('./src/bloges.html').pipe(res);
                 break;
             case '/admin':
             case '/login':
-                fs.readFile('./src/login.html', function (err, html) {
-                    if (err) throw err;
-                    res.writeHeader(200, { "Content-Type": "text/html" });
-                    res.write(html);
-                    res.end()
-                })
+                res.setHeader("Content-Type", "text/html");
+                file('./src/login.html').pipe(res);
                 // }
                 break;
             case '/admin?role=admin':
-                fs.readFile('./src/admin.html', function (err, html) {
-                    if (err) throw err;
-                    res.writeHeader(200, { "Content-Type": "text/html" });
-                    res.write(html);
-                    res.end()
-                })
+
+                res.setHeader("Content-Type", "text/html");
+                file('./src/admin.html').pipe(res);
+
                 break;
             default:
 
-                fs.readFile('./src/noFound.html', function (err, html) {
-                    if (err) throw err;
-                    res.writeHeader(200, { "Content-Type": "text/html" });
-                    res.write(html);
-                    res.end()
-                })
+                res.setHeader("Content-Type", "text/html");
+                file('./src/noFound.html').pipe(res);
+
                 break;
         }
 
